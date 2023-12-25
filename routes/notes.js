d448fd5266d0d337eb87ed3f0d3710ab59762821
Beatriz Bravo <beatz.bravo@gmail.com>
2023-12-25 14:33:53 -0700

    create get request from the db and tested using insomnia

---
+++ routes/notes.js
@@ -0,0 +1,14 @@
+const notes = require("express").Router();
+const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
+// const db = require("../db/db.json");
+
+//GET route for retrieving all notes
+notes.get("/", (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+// POST route for a new note
+
+
+module.exports = notes;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 // express
 const express = require("express");
 const path = require("path");
+//retrieve notes server
+const notes = require("./routes/notes");
 
 // DATA
 
@@ -14,6 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //static
 app.use(express.static("public"));
+//api/notes
+app.use("/api/notes", notes);
 
 // ROUTES
 // html routes - deliver pages
@@ -22,13 +26,16 @@ app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "public/index.html"))
 );
 
-//api routes
-// app.get("api/todos", (req, res) => res.json([{ title: "buy milk" }]));
+// GET / - notes page
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
 
 //404 route
 // app.get("*", (req, res) =>
 //   res.sendFile(path.join(__dirname, "public/404.html"))
 // );
+
 
 // START SERVER
 app.listen(PORT, () => console.log(`Server live on ${PORT}`));
