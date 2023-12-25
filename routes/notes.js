1e953f20f05fcc56f0d7ea56736b40a4ae76c43f
Beatriz Bravo <beatz.bravo@gmail.com>
2023-12-25 14:52:47 -0700

    imported npm uuid package

--- routes/notes.js
+++ routes/notes.js
@@ -1,4 +1,5 @@
 const notes = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
 const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
 
 //GET route for retrieving all notes
@@ -8,6 +9,9 @@ notes.get("/", (req, res) => {
 });
 
 // POST route for a new note
-
+notes.post("/", (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
 
 module.exports = notes;
