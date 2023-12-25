3d2cf1d8260386176434db2164651378e8913215
Beatriz Bravo <beatz.bravo@gmail.com>
2023-12-25 14:36:51 -0700

    link to nptes.html

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,5 @@
 const notes = require("express").Router();
 const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
-// const db = require("../db/db.json");
 
 //GET route for retrieving all notes
 notes.get("/", (req, res) => {
