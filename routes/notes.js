bd7ec1944a02ee73812d9e65bb9b586a7894a7ae
Beatriz Bravo <beatz.bravo@gmail.com>
2023-12-25 15:33:26 -0700

    deletes notes.json content

--- routes/notes.js
+++ routes/notes.js
@@ -1,10 +1,11 @@
 const notes = require("express").Router();
-const { v4: uuidv4 } = require("uuid");
-const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
+const { v4: uuidv4 } = require("uuid"); //uuid package to create unique id
+const { readFromFile, readAndAppend } = require("../helpers/fsUtils"); //helper function
 
 //GET route for retrieving all notes
 notes.get("/", (req, res) => {
   console.info(`${req.method} request received for notes`);
+  //reads file from notes.json file
   readFromFile("./db/notes.json").then((data) => res.json(JSON.parse(data)));
 });
 
@@ -12,13 +13,15 @@ notes.get("/", (req, res) => {
 notes.post("/", (req, res) => {
   console.info(`${req.method} request received for notes`);
   const note = req.body;
-  //append to notes db and create a new object with a unique ID
+  //append to notes.json and create a new object with a unique ID
+  //the request body will have title and text
   const newNote = {
     id: uuidv4(),
     title: note.title,
     text: note.text,
   };
   res.json(newNote);
+  //reads and append file with new note
   readAndAppend(newNote, "./db/notes.json");
 });
 
--- server.js
+++ server.js
@@ -16,8 +16,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //static
 app.use(express.static("public"));
-//api/notes
-app.use("/api/notes", notes);
 
 // ROUTES
 // html routes - deliver pages
@@ -35,6 +33,10 @@ app.get("/notes", (req, res) =>
 // app.get("*", (req, res) =>
 //   res.sendFile(path.join(__dirname, "public/404.html"))
 // );
+
+//ROUTES
+//GET api/notes
+app.use("/api/notes", notes);
 
 // START SERVER
 app.listen(PORT, () => console.log(`Server live on ${PORT}`));
