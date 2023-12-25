a82a8d5997134220b7c4bdf1ff287af8d89fefe9
Beatriz Bravo <beatz.bravo@gmail.com>
2023-12-25 15:03:50 -0700

    creates post request and appends to file

--- routes/notes.js
+++ routes/notes.js
@@ -5,13 +5,19 @@ const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
 //GET route for retrieving all notes
 notes.get("/", (req, res) => {
   console.info(`${req.method} request received for notes`);
-  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+  readFromFile("./db/notes.json").then((data) => res.json(JSON.parse(data)));
 });
 
 // POST route for a new note
 notes.post("/", (req, res) => {
   console.info(`${req.method} request received for notes`);
-  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+  //append to notes db and create a new object with a unique ID
+  const newNote = {
+    id: uuidv4(),
+    note: req.body,
+  };
+  res.json(newNote);
+  readAndAppend(newNote, "./db/notes.json");
 });
 
 module.exports = notes;
